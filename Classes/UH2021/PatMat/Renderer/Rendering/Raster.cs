4648656635f62ad7a4897caa7505a7d38b57db5e
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2021-03-16 12:47:02 -0400

    Abusement

---
+++ Classes/UH2021/PatMat/Renderer/Rendering/Raster.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GMath;
+using static GMath.Gfx;
+
+namespace Rendering
+{
+    /// <summary>
+    /// Represents a rasterizer object allowing to draw primitives in a specific texture.
+    /// </summary>
+    public class Raster
+    {
+        /// <summary>
+        /// Gets the specific texture this renderer is drawing at.
+        /// </summary>
+        public Texture2D RenderTarget { get; private set; }
+
+        /// <summary>
+        /// Initializes a rasterizer to use a specific texture to draw at.
+        /// </summary>
+        public Raster (Texture2D renderTarget)
+        {
+            this.RenderTarget = renderTarget;
+        }
+
+        /// <summary>
+        /// Initializes a rasterizer to use a new texture to draw at with specific dimensions.
+        /// </summary>
+        public Raster(int width, int height) : this(new Texture2D(width, height))
+        {
+        }
+
+        /// <summary>
+        /// Clears the render target with a specific color
+        /// </summary>
+        public void ClearRT(float4 color)
+        {
+            for (int i = 0; i < RenderTarget.Height; i++)
+                for (int j = 0; j < RenderTarget.Width; j++)
+                    RenderTarget.Write(j, i, color);
+        }
+
+        /// <summary>
+        /// Render a set of points. Points are assumed to be in Normalized Device Coordinates.
+        /// I.e. visible points have x and y between -1 and 1, and z between 0 and 1.
+        /// Corresponding colors are used for each point.
+        /// </summary>
+        public void DrawPoints(float3[] points, float4[] colors)
+        {
+            if (RenderTarget == null)
+                return;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                float3 pto = points[i];
+                if (pto.x < -1 || pto.x >= 1 || pto.y <= -1 || pto.y > 1 || pto.z < 0 || pto.z >= 1)
+                    continue; // clip no-visible point
+                              // Convert to image coordinates (0,0) - (Width, Height)
+                int px = (int)((pto.x * 0.5f + 0.5f) * RenderTarget.Width);
+                int py = (int)((0.5f - pto.y * 0.5f) * RenderTarget.Height);
+
+                RenderTarget.Write(px, py, colors[i]);
+            }
+        }
+
+        /// <summary>
+        /// Render a set of points. Points are assumed to be in Normalized Device Coordinates.
+        /// I.e. visible points have x and y between -1 and 1, and z between 0 and 1.
+        /// Color is used for every point.
+        /// </summary>
+        public void DrawPoints(float3[] points, float4 color)
+        {
+            float4[] colors = new float4[points.Length];
+            for (int i = 0; i < colors.Length; i++)
+                colors[i] = color;
+            DrawPoints(points, colors);
+        }
+
+        /// <summary>
+        /// Render a set of points. Points are assumed to be in Normalized Device Coordinates.
+        /// I.e. visible points have x and y between -1 and 1, and z between 0 and 1.
+        /// White color is used for every point.
+        /// </summary>
+        public void DrawPoints(float3[] points)
+        {
+            DrawPoints(points, float4(1, 1, 1, 1));
+        }
+    }
+}
---
+++ Classes/UH2021/PatMat/Renderer/Rendering/Texture2D.cs
@@ -0,0 +1,68 @@
+﻿using GMath;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Rendering
+{
+    public class Texture2D
+    {
+        public readonly int Width;
+        public readonly int Height;
+
+        private float4[,] data;
+
+        /// <summary>
+		/// Creates an empty texture 2D with a specific size.
+		/// </summary>
+        public Texture2D(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.data = new float4[height, width];
+        }
+
+        /// <summary>
+        /// Gets or sets a value in the texture at specific position.
+        /// </summary>
+        public float4 this[int px, int py]
+        {
+            get { return data[py, px]; }
+            set { data[py, px] = value; }
+        }
+
+        /// <summary>
+		/// Writes a value in the texture at specific position.
+		/// </summary>
+		public void Write(int x, int y, float4 value)
+        {
+            data[y , x] = value;
+        }
+
+        /// <summary>
+        /// Reads a value from a specific position.
+        /// </summary>
+        public float4 Read(int x, int y)
+        {
+            return data[y , x];
+        }
+
+        public void Save(string fileName)
+        {
+            FileStream fileStream = new FileStream(fileName, FileMode.Create);
+            BinaryWriter binaryWriter = new BinaryWriter(fileStream);
+            binaryWriter.Write(Width);
+            binaryWriter.Write(Height);
+            for(int py = 0; py < Height; py++)
+                for (int px = 0; px < Width; px++)
+                {
+                    binaryWriter.Write(data[py, px].x);
+                    binaryWriter.Write(data[py, px].y);
+                    binaryWriter.Write(data[py, px].z);
+                    binaryWriter.Write(data[py, px].w);
+                }
+            binaryWriter.Close();
+        }
+    }
+}
